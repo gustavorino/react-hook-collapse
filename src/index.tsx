b93da605ad338fa4a081559cb6076a655d6a1d0f
Gustavo Bremm <gustavo@base.digital>
2021-03-27 21:43:19 -0300

    fix: submit static examples

--- src/index.tsx
+++ src/index.tsx
@@ -2,34 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AiFillBug, AiFillGithub } from 'react-icons/ai';
 import Example1 from './examples/Example1';
+
 function Main() {
   return (
-    <main className="p-5">
-      <div className="container mx-auto p-5">
-        <h1 className="text-4xl my-3">React Hook Collapse</h1>
-        <code className="bg-black bg-opacity-20 p-3 inline-block my-3 text-sm text-opacity-80 text-black">
-          npm install react-hook-collapse
-        </code>
-        <div className="flex space-x-3">
-          <a
-            className="text-sienna inline-flex items-center border border-black rounded-md p-2"
-            target="_blank"
-            href="https://github.com/gustavorino/react-hook-collapse"
-          >
-            <AiFillGithub className="mr-2 text-black" />
-            Github
-          </a>
-          <a
-            className="text-sienna inline-flex items-center border border-black rounded-md p-2"
-            target="_blank"
-            href="https://github.com/gustavorino/react-hook-collapse/issues/new"
-          >
-            <AiFillBug className="mr-2 text-black" />
-            Issues
-          </a>
+    <main className="">
+      <div className="bg-sienna text-cream">
+        <div className="container mx-auto p-5 ">
+          <h1 className="text-4xl my-3">React Hook Collapse</h1>
+          <code className="bg-black bg-opacity-20 p-3 inline-block my-3 text-sm text-opacity-80 text-white">
+            npm install react-hook-collapse
+          </code>
+          <div className="flex space-x-3">
+            <a
+              className="text-white inline-flex items-center border border-white border-opacity-30 rounded-md p-2"
+              target="_blank"
+              href="https://github.com/gustavorino/react-hook-collapse"
+            >
+              <AiFillGithub className="mr-2 text-tangerine" />
+              Github
+            </a>
+            <a
+              className="text-white inline-flex items-center border border-white border-opacity-30 rounded-md p-2"
+              target="_blank"
+              href="https://github.com/gustavorino/react-hook-collapse/issues/new"
+            >
+              <AiFillBug className="mr-2 text-tangerine" />
+              Issues
+            </a>
+          </div>
         </div>
-        <h2 className="text-3xl text-yellow-700 mt-5">Example #1</h2>
-        <Example1></Example1>
+      </div>
+      <div className="container mx-auto p-5">
+        <h2 className="text-3xl text-yellow-700 mt-12 mb-8">Example #1</h2>
+        <Example1 />
       </div>
     </main>
   );
